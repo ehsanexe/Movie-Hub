fd08d5e80827ae31180423c488a4b74d274e9116
ehsanexe <ehsanexe01@gmail.com>
2020-09-15 21:28:43 +0500

    search working

--- moviehub/src/Search.js
+++ moviehub/src/Search.js
@@ -2,22 +2,9 @@ import React , { Component } from 'react'
 import axios from 'axios'
 
 class Search extends Component {
-    
-    componentDidMount(){
-        axios.get('http://www.omdbapi.com/?t=young+sheldon&apikey=cb3a98a0')
-        .then(res => {
-            console.log(res);
-            this.setState({
-                movieName: res.data.Title,
-                image:res.data.Poster,
-                rating: res.data.imdbRating,
-                plot: res.data.Plot,
-                genre: res.data.Genre
-            })
-        })
-    }
 
     state = {
+        search:"",
         movieName: "",
         image:"",
         rating: "",
@@ -25,18 +12,43 @@ class Search extends Component {
         genre:""
     }
 
+    HandleChange = (e) => {
+        this.setState({
+                search: e.target.value 
+            })
+        
+        console.log(this.state.search)
+        
+    }
+
+    HandleSubmit = (e) => {
+        e.preventDefault();
+        const input = this.state.search;
+        axios.get(`http://www.omdbapi.com/?t=${this.state.search}&apikey=cb3a98a0`)
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    movieName: res.data.Title,
+                    image:res.data.Poster,
+                    rating: res.data.imdbRating,
+                    plot: res.data.Plot,
+                    genre: res.data.Genre
+                })
+            })
+    }
+
     render(){
         return(
             <div>
                 <form onSubmit={this.HandleSubmit}>
                     <label htmlFor="search">Search:</label>
-                    <input type="text" id="searchInput" onChange={this.HandleChange} />
-                    <button>Submit</button>
+                    <input type="text" onChange={this.HandleChange} />
+                    <button type="submit">Submit</button>
                 </form>
 
                 <div className="movie">
                     <h1>{this.state.movieName}</h1>
-                    <img src={this.state.image} alt="image missing" />
+                    <img src={this.state.image} alt=" missing" />
                     <p>Rating:{this.state.rating}</p>
                     <p>Plot:{this.state.plot}</p>
                     <p>Genre:{this.state.genre}</p>
